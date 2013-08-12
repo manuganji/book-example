88947a1c7dfd2615144e302b2941fad767eeab07
Harry <hjwp2@cantab.net>
2013-08-12 14:06:03 +0100

    spike a class-based-view in

--- lists/forms.py
+++ lists/forms.py
@@ -4,20 +4,22 @@ from django.forms.fields import HiddenInput
 
 class ItemForm(ModelForm):
 
-    def __init__(self, parent_list, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['list'].initial = parent_list
-        self.fields['text'].error_messages['required'] = "You can't have a blank item"
-        self.data['list'] = parent_list.id
-
-    def validate_unique(self):
-        super().validate_unique()
-        if '__all__' in self.errors:
-            self._update_errors({'text': ["You've already got this item in your list"]})
-            del self.errors['__all__']
-
     class Meta:
         model = Item
         fields =  ('list', 'text')
         widgets = {'list': HiddenInput}
+
+    def __init__(self, parent_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['list'].initial = parent_list
+        self.fields['text'].error_messages['required'] = "You can't have an empty list item"
+        self.data['list'] = parent_list.id
+
+
+    def validate_unique(self):
+        super().validate_unique()
+        if '__all__' in self.errors:
+            self._update_errors({'text': ["You've already got this in your list"]})
+            del self.errors['__all__']
+
 
--- lists/urls.py
+++ lists/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
+from lists import views as list_views
 
 urlpatterns = patterns('',
     url(r'^(.+)/$', 'lists.views.view_list', name='view_list'),
+    #url(r'^(?P<list_id>.+)/$', list_views.ListViewAndAddItemView.as_view(), name='view_list'),
     url(r'^new$', 'lists.views.new_list', name='new_list'),
 )
