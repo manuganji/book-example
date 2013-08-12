29482bf76f16b77e4a8ef87d8aacff934b8e6e2c
Harry <hjwp2@cantab.net>
2013-08-12 20:01:02 +0100

    start again on forms

---
+++ lists/forms.py
@@ -0,0 +1,22 @@
+from django.core.exceptions import ValidationError
+from django import forms
+
+from lists.models import Item
+
+class ItemForm(forms.models.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        empty_error = "You can't have an empty list item"
+        self.fields['text'].error_messages['required'] = empty_error
+
+    class Meta:
+        model = Item
+        fields = ('text', 'list')
+        widgets = {
+            'text': forms.fields.TextInput(
+                attrs={'placeholder': 'Enter a to-do item'}
+            ),
+            'list': forms.fields.HiddenInput(),
+        }
+
