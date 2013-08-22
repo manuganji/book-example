9485b5dc38f96369b0f5d3e4785c74eb9bdbe5c8
Harry <hjwp2@cantab.net>
2013-08-22 20:44:29 +0100

    move view list function (no-op)

--- lists/views.py
+++ lists/views.py
@@ -7,11 +7,6 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request, list_id):
-    list = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list})
-
-
 def new_list(request):
     list = List.objects.create()
     try:
@@ -22,7 +17,13 @@ def new_list(request):
     return redirect('/lists/%d/' % (list.id,))
 
 
+def view_list(request, list_id):
+    list = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list})
+
+
 def add_item(request, list_id):
     list = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list)
     return redirect('/lists/%d/' % (list.id,))
+
