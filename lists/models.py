b8427cd34423cab096c35acacef6cb9902bd62a4
Harry <hjwp2@cantab.net>
2013-08-29 10:27:33 +0100

    correct unique_together constrain. --ch09l031--

--- lists/models.py
+++ lists/models.py
@@ -4,8 +4,11 @@ class List(models.Model):
     pass
 
 class Item(models.Model):
-    text = models.TextField(unique=True)
+    text = models.TextField()
     list = models.ForeignKey(List)
+
+    class Meta:
+        unique_together = ('list', 'text')
 
 
     def save(self, *args, **kwargs):
