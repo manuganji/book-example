a63173032969fcdf572641e0aad5e7df5fa2b36a
Harry <hjwp2@cantab.net>
2013-08-29 17:59:50 +0100

    remove old unit test

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,4 @@
 from django.core.urlresolvers import resolve
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 from django.test import Client, TestCase
 from django.utils.html import escape
 
@@ -15,13 +13,6 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertMultiLineEqual(response.content.decode(), expected_html)
 
 
     def test_home_page_renders_home_template_with_form(self):
